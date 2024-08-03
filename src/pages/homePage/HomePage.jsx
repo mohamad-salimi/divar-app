7ab26415069dea0c70350698237bb38b01d31971
Mohamad Salimi <mohamadsalimi151@gmail.com>
2024-08-03 15:38:57 +0330

    feat: develop home page and category sidebar

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,5 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import Loader from "components/module/loader/Loader";
+import Main from "components/template/main/Main";
+import Sidebar from "components/template/sidebar/Sidebar";
+import { getCategory } from "services/admin";
+import { getAllPosts } from "services/user";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  const { data: posts, isLoading: postsLoading } = useQuery(
+    ["post-list"],
+    getAllPosts
+  );
+  const { data: categories, isLoading: categoryLoading } = useQuery(
+    ["get-categories"],
+    getCategory
+  );
+
+  return (
+    <>
+      {postsLoading || categoryLoading ? (
+        <Loader />
+      ) : (
+        <div style={{ display: "flex", columnGap: "100px" }}>
+          <Sidebar categories={categories} />
+          <Main posts={posts} />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default HomePage;
